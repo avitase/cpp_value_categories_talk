a2fec2c641fa698236d931b839393cbe729a27ea
Nis Meinert <mail@nismeinert.de>
2019-10-20 22:32:38 +0200

    add notes about auto && vs decltype(auto)

---
+++ snippets/snippet6.cpp
@@ -0,0 +1,9 @@
+#include <functional>
+
+template <typename Iter, typename Callable, typename... Args>
+void foreach (Iter current, Iter end, Callable op, const Args &... args) {
+    while (current != end) {
+        std::invoke(op, args..., *current);
+        ++current;
+    }
+}
