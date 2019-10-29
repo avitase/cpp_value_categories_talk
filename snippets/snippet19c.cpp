2270d997c6f9af96338a7a29790854c4c2eadec4
Nis Meinert <nis.meinert@gmail.com>
2019-10-29 18:00:05 +0100

    add more broken NRVO examples

---
+++ snippets/snippet19c.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+struct Resource {
+    Resource() {}
+    Resource(const Resource&) { std::cout << 'a'; }
+};
+struct ResourceManager {
+    Resource resource;
+
+    template <typename T>
+    auto visit(T visitor) -> decltype(visitor(resource)) {
+        using R = std::invoke_result_t<T, Resource&>;
+        if constexpr (decltype(auto) result = visitor(resource);
+                      std::is_same_v<R, Resource&&>) {
+            return std::move(result);
+        } else {
+            return result;
+        }
+    }
+};
+struct Target {
+    Target(const Resource&) { std::cout << 'b'; }
+    Target(Resource&&) { std::cout << 'c'; }
+};
+
+int main() {
+    ResourceManager rm;
+    Target(rm.visit([](Resource &r) -> Resource& { return r; }));
+    Target(rm.visit([](Resource &r) -> Resource { return r; }));
+    Target(rm.visit([](Resource &r) -> Resource&& { return std::move(r); }));
+}
