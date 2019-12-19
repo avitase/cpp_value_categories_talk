8e7dd4bb23eab84e29d1d29d22ccd81eba51d91a
Nis Meinert <nis.meinert@gmail.com>
2019-12-19 15:25:54 +0100

    rehash snippets

--- snippets/snippet24.asm
+++ snippets/snippet24.asm
@@ -1,2 +1,20 @@
-# g92 -03 -std=c++2a
-  | <Compilation failed>
+# g92 -O3 -std=c++2a
+  | f():
+ 9|   push r12
+ 9|   mov r12, rdi
+230|   call S::S() [complete object constructor]
+230|   lea rdi, [r12+1]
+230|   call S::S() [complete object constructor]
+ 9|   mov rax, r12
+ 9|   pop r12
+ 9|   ret
+  | g1():
+10|   push r12
+10|   mov r12, rdi
+10|   sub rsp, 16
+230|   lea rdi, [rsp+14]
+230|   call S::S() [complete object constructor]
+230|   lea rdi, [rsp+15]
+230|   call S::S() [complete object constructor]
+10|   lea rsi, [rsp+14]
+[...]
